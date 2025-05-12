710760ee823051bdee9581acab6bca35510678d8
safwan72 <safwan4872@gmail.com>
2025-05-12 20:50:56 +0600

    Integrated Frontend

--- django_chat_app/main/views.py
+++ django_chat_app/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+
+API_KEY=''
+
+def chatbot(request):
+    if request.method=='POST':
+        message=request.POST.get('message')
+        response='HI IM CHAT'
+        return JsonResponse({'message':message,'response':response})
+    return render(request,'chatbot.html')
